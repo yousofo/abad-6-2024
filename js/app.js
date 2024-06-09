25b0ced129bbde564b8c11cf443896a5c5655e56
yousefn <jon772002@gmail.com>
2024-06-10 01:59:26 +0300

    swiper resp

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ import Swiper from "./swiper-bundle.esm.browser.min.js";
 const swiper1 = new Swiper(".swiper1", {
   loop: true,
   speed: 700,
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 0,
   navigation: {
     nextEl: '.swiper1-next',
     prevEl: '.swiper1-prev',
   },
-  // breakpoints: {
-  //   640: {
-  //     spaceBetween: 50,
-  //   }
-  // },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    }
+  },
 });
