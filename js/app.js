613adbe59a896ec8b7a47a589e6c62d3f22da679
yousefn <jon772002@gmail.com>
2024-06-11 01:44:10 +0300

    dual range placeholder

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ window.onload = function () {
     max: 500,
     values: [75, 300],
     slide: function (event, ui) {
-      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+      $("#amount").val("ريال سعودي" + ui.values[0] + " - ريال سعودي" + ui.values[1]);
     }
   });
   $("#amount").val("$" + $("#slider-range").slider("values", 0) +
