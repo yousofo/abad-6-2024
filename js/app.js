a55c0fb8ad170626bcb354e689525dcf7b4941b8
yousefn <jon772002@gmail.com>
2024-06-11 15:46:51 +0300

    filter search update

--- js/app.js
+++ js/app.js
@@ -32,7 +32,16 @@ const swiper2 = new Swiper(".swiper2", {
     }
   },
 });
+
+const filterOptions = {
+  mode: "rows",
+  type: "all"
+}
+const allDataRows = $(".courses-rows tbody tr")
+const allDataCards = $(".courses-cards figure")
+
 window.onload = function () {
+
   //toggle nav list
   document.onclick = () => $(".nav-list-drop").hide()
   $(".toggle-nav-list").on("click", (e) => {
@@ -46,16 +55,55 @@ window.onload = function () {
   })
 
   //toggle courses preview mode
-  $(".courses-preview-mode").on("click",()=>{
+  $(".courses-preview-mode").on("click", () => {
     $(".courses-preview-mode").toggleClass("active")
-    console.log($(".courses-preview-mode.active").data("mode"))
-    if($(".courses-preview-mode.active").data("mode")=="cards"){
-      console.log("cards")
+    filterOptions.mode = $(".courses-preview-mode.active").data("mode")
+    updateFilterUI(filterOptions.type)
+    if ($(".courses-preview-mode.active").data("mode") == "cards") {
       $(".courses-rows").hide(0)
       $(".courses-cards").show(300)
-    }else{
+    } else {
       $(".courses-cards").hide(0)
       $(".courses-rows").show(300)
     }
   })
+
+  //courses filter
+
+  function updateFilterUI(current){
+    if (filterOptions.mode == "cards") {
+      if (current == "all") {
+        $(".courses-cards").html(allDataCards)
+      } else {
+        const newData = allDataCards.filter((i, ele) => $(ele).data("filter") == filterOptions.type)
+        $(".courses-cards").html(newData)
+      }
+    } else {
+      if (current == "all") {
+        $(".courses-rows tbody").html(allDataRows)
+      } else {
+        const newData = allDataRows.filter((i, ele) => $(ele).data("filter") == filterOptions.type)
+        $(".courses-rows tbody").html(newData)
+      }
+    }
+  }
+
+  //select all list items and loop over them
+  $(".courses-filter li").each((i, e) => {
+    let current = $(e)
+    let currentData = current.data("filter")
+    //add click event listener to  each item
+    current.on("click", () => {
+      //remove active class from all siblings
+      $(".courses-filter li").removeClass("active")
+      //add class to the clicked element
+      current.addClass("active")
+      //add filter type info to filterOptions
+      filterOptions.type = current.data("filter")
+      //update ui based on which mode is active
+      updateFilterUI(currentData)
+
+    })
+  })
+
 };
